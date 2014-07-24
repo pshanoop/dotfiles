997c1ede45b1c984b625b21c6dac6b281c6a6424
Hugo Osvaldo Barrera <hugo@barrera.io>
2014-07-24 17:55:20 -0300

    Updated bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-
-# This script will enable some basic (and not-so-basic) systemd service
-# so as to have a running X enviroment. Once it has started, you can run
-# `systemctl --user enable` ??.services to enable more services.  I use
-# this script to get new installations (new machine) up and running quicky
-# after cloning my home repo, and symlinking everything using homesync.py.
+#
+# Creates empty directories which need to exists but apps won't
+# auto-create.
+#
 
 mkdir -p "$HOME/.local/share/maildir/Personal"
 mkdir -p "$HOME/.local/share/urxvt"
-pacaur -S python2-docopt
+mkdir -p "$HOME/.cache/ssh"
