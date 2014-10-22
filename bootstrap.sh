d986020c15355c3ca98db08164363bb940eedfc7
Hugo Osvaldo Barrera <hugo@barrera.io>
2014-10-22 00:27:22 -0300

    Deleted obsolete files.

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Creates empty directories which need to exists but apps won't
-# auto-create.
-#
-
-mkdir -p "$HOME/.local/share/maildir/Personal"
-mkdir -p "$HOME/.local/share/urxvt"
-mkdir -p "$HOME/.cache/ssh"
