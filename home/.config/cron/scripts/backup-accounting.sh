0b8a553f0f0e305736de717faf2f4c35ef405714
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-03-10 06:15:53 -0300

    cron scripts: Fix bashisms.

--- home/.config/cron/scripts/backup-accounting.sh
+++ home/.config/cron/scripts/backup-accounting.sh
@@ -4,6 +4,6 @@ if [ $(hostname -s) != athena ]; then exit 1; fi;
 
 cd /home/hugo/accounting/
 
-if [ $(git status --porcelain homebank.xhb | wc -l) == 1 ]; then
+if [ $(git status --porcelain homebank.xhb | wc -l) = 1 ]; then
   git commit -qm 'Automatic cron commit (homebank only).' homebank.xhb
 fi
