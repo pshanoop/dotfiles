888cf8aa59b3f215d58e24a1f94fae3a8aba9734
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-03-23 23:08:56 -0300

    backup-photos: Send an email with backup results.

--- home/.config/cron/scripts/backup-photos.sh
+++ home/.config/cron/scripts/backup-photos.sh
@@ -11,21 +11,23 @@ set -u
 
 if [ $(hostname -s) != hyperion ]; then exit 1; fi;
 
-FILE_DIR=/home/hugo/photos
-META_DIR=/home/hugo/.local/share/tarsnap/index
-CACHE_DIR=/home/hugo/.local/share/tarsnap/cache
-WHEN=$(date +%Y-%m-%dT%H%M%S%z)
-KEY=/home/hugo/priv/keys/tarsnap/hyperion-w.key
+(
+  FILE_DIR=/home/hugo/photos
+  META_DIR=/home/hugo/.local/share/tarsnap/index
+  CACHE_DIR=/home/hugo/.local/share/tarsnap/cache
+  WHEN=$(date +%Y-%m-%dT%H%M%S%z)
+  KEY=/home/hugo/priv/keys/tarsnap/hyperion-w.key
 
-# Update the file index.
-find $FILE_DIR | sort > $META_DIR/photos
-# TODO: Only commit if there's changes. Also, do a log --stat.
-git -C $META_DIR add $META_DIR/photos
-git -C $META_DIR commit -m "Update photos index file for $WHEN." \
-  > /dev/null || true
+  # Update the file index.
+  find $FILE_DIR | sort > $META_DIR/photos
+  # TODO: Only commit if there's changes. Also, do a log --stat.
+  git -C $META_DIR add $META_DIR/photos
+  git -C $META_DIR commit -m "Update photos index file for $WHEN." \
+    > /dev/null || true
 
-# Perform the backup itself.
-cd $FILE_DIR/..
-tarsnap -c --keyfile $KEY --cachedir $CACHE_DIR -f photos-$WHEN \
-  --exclude Canvas --exclude Unsorted --one-file-system \
-  --print-stats --humanize-numbers photos
+  # Perform the backup itself.
+  cd $FILE_DIR/..
+  tarsnap -c --keyfile $KEY --cachedir $CACHE_DIR -f photos-$WHEN \
+    --exclude Canvas --exclude Unsorted --one-file-system \
+    --print-stats --humanize-numbers photos
+) 2>&1 | mail -s "Hyperion daily photo backup" hugo@barrera.io
