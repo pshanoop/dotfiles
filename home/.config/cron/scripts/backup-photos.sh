863baf8deae8d3150d4514b4f5ffedbdf0f88139
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-12-15 11:11:21 -0300

    Simplify photos backup script

--- home/.config/cron/scripts/backup-photos.sh
+++ home/.config/cron/scripts/backup-photos.sh
@@ -8,14 +8,14 @@ set -u
 if [ $(hostname -s) != hyperion ]; then exit 1; fi;
 
 (
-  FILE_DIR=/home/hugo/photos
+  PARENT=/home/hugo/
+  FILE=photos
   CACHE_DIR=/home/hugo/.local/share/tarsnap/cache
   WHEN=$(date +%Y-%m-%dT%H%M%S%z)
   KEY=/home/hugo/priv/keys/tarsnap/barrera.io/hyperion.w.key
 
-  # Perform the backup itself.
-  cd $FILE_DIR/..
-  tarsnap -c --keyfile $KEY --cachedir $CACHE_DIR -f photos-$WHEN \
+  cd $PARENT
+  tarsnap -c --keyfile $KEY --cachedir $CACHE_DIR -f $FILE-$WHEN \
     --exclude Canvas --exclude Unsorted --exclude Jolla --one-file-system \
-    --print-stats --humanize-numbers photos
+    --print-stats --humanize-numbers $FILE
 ) 2>&1 | mail -s "Daily Photo Backup" hugo@barrera.io
