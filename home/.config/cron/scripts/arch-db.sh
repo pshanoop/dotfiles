759e0794a45a39e6bed4b5e7032ddcd93dfc036a
Hugo Osvaldo Barrera <hugo@barrera.io>
2015-12-16 00:49:19 -0300

    crontab(1): Delete obsolete tasks

--- home/.config/cron/scripts/arch-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [ $(hostname -s) != hyperion ]; then exit 1; fi;
-
-cd /srv/http/default/archlinux
-/usr/bin/repose --files hugo.db *.xz
--- home/.config/cron/scripts/backup-accounting.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ $(hostname -s) != athena ]; then exit 1; fi;
-
-cd /home/hugo/accounting/
-
-if [ $(git status --porcelain homebank.xhb | wc -l) = 1 ]; then
-  git commit -qm 'Automatic cron commit (homebank only).' homebank.xhb
-fi
