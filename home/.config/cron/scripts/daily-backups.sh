a099167ab5499e4f68ce06a15c94091c3d46a885
Hugo Osvaldo Barrera <hugo@barrera.io>
2016-02-09 06:25:44 -0300

    Merge backup scripts into one
    Aside from deduplicating a lot of code, I only get one daily email,
    instead of three (ugh).

---
+++ home/.config/cron/scripts/daily-backups.sh
@@ -0,0 +1,49 @@
+
+#!/bin/sh
+#
+# Performs several backup tasks (into tarsnap)
+
+set -e
+set -u
+
+if [ $(hostname -s) != hyperion ]; then exit 1; fi;
+
+CACHE_DIR=/home/hugo/.local/share/tarsnap/cache
+KEY=/home/hugo/priv/keys/tarsnap/barrera.io/hyperion.w.key
+
+SHARED_FLAGS="--print-stats --humanize-numbers --one-file-system"
+COMMAND="tarsnap -c --keyfile $KEY --cachedir $CACHE_DIR $SHARED_FLAGS"
+
+WHEN=$(date +%Y-%m-%dT%H%M%S%z)
+
+(
+# ~~~Photos~~~
+  echo -e "\n~~~ Photos ~~~"
+  FILE=photos
+
+  cd $HOME
+  $COMMAND -f photos-$WHEN \
+    --exclude Canvas \
+    --exclude Unsorted \
+    --exclude Jolla \
+    $FILE
+
+# ~~~Bitcoin~~~
+  echo -e "\n~~~ Bitcoin Wallet ~~~"
+  FILE=wallet.dat
+
+  cd $HOME/.bitcoin/
+  $COMMAND -f bitcoin-$WHEN $FILE
+
+# ~~~Mailbox~~~
+  echo -e "\n~~~ Mailbox ~~~"
+  FILE=maildir
+
+  cd $HOME/.local/share/
+  $COMMAND -f maildir-$WHEN \
+    --exclude .notmuch \
+    --exclude INBOX.Trash \
+    --exclude INBOX.IDF \
+    $FILE
+
+) 2>&1 | mail -s "Hyperion's Daily Backup" hugo@barrera.io
