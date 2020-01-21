6182b9d312cc4c008c42a06b8812d4888150ee18
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-01-21 05:17:22 +0100

    X: Configure new laptop trackpad

--- home/.config/Xorg/configure.sh
+++ home/.config/Xorg/configure.sh
@@ -7,9 +7,11 @@ xrandr --dpi 144
 
 # Enable click-to-tap:
 xinput set-prop "DELL08AF:00 06CB:76AF Touchpad" "libinput Tapping Enabled" 1
+xinput set-prop "SynPS/2 Synaptics TouchPad" "libinput Tapping Enabled" 1
 
 # Increase touchpad sensitivity:
 xinput set-prop "DELL08AF:00 06CB:76AF Touchpad" "Coordinate Transformation Matrix" 2 0 0 0 2 0 0 0 1
+xinput set-prop "SynPS/2 Synaptics TouchPad" "Coordinate Transformation Matrix" 2 0 0 0 2  0 0 0 1
 
 # Decrease mouse sensitivity:
 xinput --set-prop "pointer:Razer Razer Lancehead Wireless" "Coordinate Transformation Matrix" 0.55 0 0 0 0.55 0 0 0 1
