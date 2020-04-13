12e3c53533b36126b7163bf91bbb47a839a23919
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-04-13 20:18:23 +0200

    Remove unused zsh plugin: RVM

--- home/.config/zsh/plugins/rvm/rvm.plugin.zsh
+++
@@ -1,7 +0,0 @@
-# OMZ/Antigen plugin that loads rvm if it's locally available
-
-if [ -d $HOME/.rvm ]; then
-  export rvm_ignore_gemrc_issues=1
-  export PATH="$PATH:$HOME/.rvm/bin"
-  source "$HOME/.rvm/scripts/rvm"
-fi
