8a417de2f7d25e71e710d0c2278b6d4710aa4eb6
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-04-21 10:42:47 +0200

    sh: Use lsd (ls deluxe) as an alias for ls

--- home/.config/sh/aliases.sh
+++ home/.config/sh/aliases.sh
@@ -2,11 +2,7 @@
 # ~/.aliases
 #
 
-if ls --color > /dev/null 2>&1; then
-  alias ls='ls -Fh --color=auto'
-else
-  alias ls='ls -Fh'
-fi
+alias ls=lsd
 alias ll='ls -l'
 alias la='ls -la'
 alias grep="grep --color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn"
