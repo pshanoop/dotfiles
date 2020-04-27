08fa57532bd03882a65dd4150b2ea8cfafaf1695
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-04-27 18:46:01 +0200

    Use `lsd --tree` as a `tree` replacement

--- home/.config/sh/aliases.sh
+++ home/.config/sh/aliases.sh
@@ -6,15 +6,14 @@ alias ls="lsd"
 alias ll="lsd -l"
 alias la="lsd -A"
 alias l="lsd -1"
+alias tree='lsd --tree'
 
 alias grep="grep --color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-dir=.hg --exclude-dir=.svn"
 alias sudo='/usr/bin/sudo '
-alias tree='tree -C'
 alias less='less -rX'
 alias open='xdg-open'
 alias fmt='fmt -w 79'
 alias ssh='TERM=xterm-256color ssh'
-alias tree='tree -C'
 alias p='git push -u origin HEAD'
 alias u='git fetch && git rebase --autostash origin/$(git branch --show-current)'
 alias tmux='/usr/bin/tmux -f ~/.config/tmux/tmux.conf'
