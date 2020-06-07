1449340171ea12f17ed999305a9a2f01a4d6b05a
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-06-07 13:27:26 +0200

    firefox: Switch bad to good old about:blank
    I tried using these shortcuts, but my hand are usually on my keyboard,
    so they're just slow and pointless for me. Omnibar auto completion
    continues to be faster.
    
    Maybe it's decades of memory-muscle?

--- home/.mozilla/firefox/default/user.js
+++ home/.mozilla/firefox/default/user.js
@@ -35,20 +35,10 @@ user_pref("browser.sessionstore.warnOnQuit", true);
 // Switch tabs in-order with Ctrl+Tab
 user_pref("browser.ctrlTab.recentlyUsedOrder", false);
 
+// Use about:blank as a "homepage".
+user_pref("browser.startup.homepage", "about:blank");
+// And use the homepage as a newtab page too.
 user_pref("browser.startup.page", 3);
-
-// These "highlights" are just rangom pages:
-user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
-user_pref("browser.newtab.preload", false);
-
-// Redundant and unused, that's what the main bar is for:
-user_pref("browser.newtabpage.activity-stream.showSearch", false);
-
-// Pinned links in the home page.
-user_pref(
-  "browser.newtabpage.pinned",
-  '[{"url":"https://www.fastmail.com/","label":"fastmail","customScreenshotURL":"https://www.fastmail.com/images/press/logos/fastmail-icon.png"},{"url":"https://news.ycombinator.com/","label":"hn","customScreenshotURL":"https://news.ycombinator.com/favicon.ico"},{"url":"https://www.notion.so/","label":"notion","customScreenshotURL":"https://www.notion.so/images/logo-ios.png"},{"url":"https://web.whatsapp.com/","label":"web.whatsapp","customScreenshotURL":"https://cdn0.iconfinder.com/data/icons/social-media-square-4/1024/whatsapp-512.png"},{"url":"https://translate.google.com/","label":"translate.google","customScreenshotURL":"https://www.shareicon.net/data/2017/04/11/883747_text_512x512.png"},{"url":"https://stackoverflow.com/","label":"so","customScreenshotURL":"https://cdn3.iconfinder.com/data/icons/inficons/512/stackoverflow.png"},{"url":"https://app.slack.com/client/T0ACV8FAN","label":"app.slack","customScreenshotURL":"https://a.slack-edge.com/80588/marketing/img/meta/slack_hash_256.png"},{"url":"https://calendar.google.com/calendar/r","label":"Calendar","customScreenshotURL":"https://cdn2.iconfinder.com/data/icons/fletro-long-shadow-google-service/512/Google_Calendar.png"}]'
-);
 
 // Position of UI elements in the toolbar.
 user_pref(
