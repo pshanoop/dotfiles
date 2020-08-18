ea57a6df3be00dbd18f18788d402b9147f243f9b
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-08-18 22:01:46 +0200

    fmt: Remove alias
    Haven't used this in ages.

--- home/.config/sh/aliases.sh
+++ home/.config/sh/aliases.sh
@@ -19,7 +19,6 @@ alias grep="grep --color=auto --exclude-dir=.cvs --exclude-dir=.git --exclude-di
 alias sudo='/usr/bin/sudo '
 alias less='less -rX'
 alias open='xdg-open'
-alias fmt='fmt -w 79'
 alias ssh='TERM=xterm-256color ssh'
 alias p='git push -u origin HEAD'
 alias u='git fetch && git rebase --autostash origin/$(git branch --show-current)'
