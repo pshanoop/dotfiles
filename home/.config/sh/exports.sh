57d8a823df45384e7ad61d8c01a4970260940f8b
Hugo Osvaldo Barrera <hugo@barrera.io>
2020-12-03 16:57:49 +0100

    flatpak: Make GTK apps render properly

--- home/.config/sh/exports.sh
+++ home/.config/sh/exports.sh
@@ -26,7 +26,9 @@ export _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=on"
 export _JAVA_OPTIONS="-Dswing.aatext=true"
 
 # xdg-desktop-portal will use the right backed if this is exported:
-export XDG_CURRENT_DESKTOP=sway
+# "GNOME" is required for GTK applications to work in flatpak and alike.
+# See https://github.com/flatpak/flatpak/issues/2861#issuecomment-735476004
+export XDG_CURRENT_DESKTOP=sway;GNOME
 
 # Other defaults
 export BROWSER=firefox
