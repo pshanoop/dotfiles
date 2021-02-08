34a36381c21dd18bd9eb2da7b9e5dd1db5042359
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-02-08 23:05:11 +0100

    nvim: Configure folding

--- home/.config/nvim/lua/tree-sitter.lua
+++ home/.config/nvim/lua/tree-sitter.lua
@@ -9,3 +9,12 @@ require'nvim-treesitter.configs'.setup {
     enable = true,
   },
 }
+
+-- Use folding from treesitter.
+--
+-- I don't _really_ use folding, but since I contributed the Vue treesitter
+-- configs, I had to set it up, and it's worth leaving since I'm curious to
+-- find uses fold folding.
+vim.cmd("set foldmethod=expr foldexpr=nvim_treesitter#foldexpr()")
+-- Open files unfolded. See https://stackoverflow.com/a/23672376
+vim.cmd("autocmd BufWinEnter * silent! :%foldopen!")
