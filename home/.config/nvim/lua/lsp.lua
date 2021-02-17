df143f55c70d82ef6e18ea28a5eef709b271b0e7
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-02-18 00:09:26 +0100

    nvim: Configure keybindings for LSP actions

--- home/.config/nvim/lua/lsp.lua
+++ home/.config/nvim/lua/lsp.lua
@@ -1,5 +1,24 @@
 local nvim_lsp = require('lspconfig')
 
-nvim_lsp.jedi_language_server.setup{}
-nvim_lsp.rust_analyzer.setup{}
-nvim_lsp.tsserver.setup{}
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+
+  local opts = { noremap=false, silent=false }
+  -- buf_set_keymap('n', '<space>D', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  -- buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', '<Leader>h', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<Leader>r', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+end
+
+-- Use a loop to conveniently both setup defined servers and map buffer local
+-- keybindings when the language server attaches:
+local servers = {
+  "jedi_language_server",
+  "rust_analyzer",
+  "tsserver",
+  "vuels",
+}
+for _, lsp in ipairs(servers) do
+  nvim_lsp[lsp].setup { on_attach = on_attach }
+end
