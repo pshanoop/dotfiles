7645dc6209885d748a1d3b9a9154dcbbd7500d5a
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-03-18 08:32:33 +0100

    nvim: Add shortcuts to switch vsplits<>hsplits

--- home/.config/nvim/lua/keymap.lua
+++ home/.config/nvim/lua/keymap.lua
@@ -1,11 +1,18 @@
--- TODO: Exclude current file from prompt
--- See https://github.com/junegunn/fzf.vim/issues/695
-
--- Ctrl-P for file navigation
--- TODO: It'd be great if matches in pwd are weighted higher.
+-- Switch from vertical split into horizontal split
 vim.api.nvim_set_keymap(
   'n',
-  '<c-p>',
-  [[(len(system('git rev-parse')) ? ':Files' : ':GFiles')."\<cr>"]],
-  { noremap = true, silent = true, expr = true}
+  '<Leader>th',
+  "<C-w>t<C-w>K",
+  { noremap = true, silent = false, expr = false}
 )
+
+-- Switch from horitonzal split into vertical split
+vim.api.nvim_set_keymap(
+  'n',
+  '<Leader>tv',
+  "<C-w>t<C-w>H",
+  { noremap = true, silent = false, expr = false}
+)
+
+-- Ctrl-w followed by H, J, K or L will move the current window to the far
+-- left, bottom, top or right respectively (same directions as usual).
