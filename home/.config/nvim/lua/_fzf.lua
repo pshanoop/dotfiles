7ad881d46ec8e93743fc8f4a303cd3434447cd67
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-05-09 22:01:10 +0200

    nvim: Refine and unify both fzf commands
    This has been long overdue: both of them had caveats, so they were
    complementary, and I used both. This rewrite is a single function that
    covers all my use cases.

--- home/.config/nvim/lua/_fzf.lua
+++ home/.config/nvim/lua/_fzf.lua
@@ -1,3 +1,5 @@
+local M = {}
+
 -- Properly position the fzf prompt:
 vim.g.fzf_layout = { down = '40%' }
 vim.g.fzf_preview_window = {}
@@ -5,26 +7,54 @@ vim.g.fzf_preview_window = {}
 -- TODO: Exclude current file from prompt
 -- See https://github.com/junegunn/fzf.vim/issues/695
 
--- Ctrl-P for file navigation
--- Only shows git-tracked files is in a git repo.
+function M.show_fuzzy_finder()
+  local handle = io.popen("git rev-parse --show-toplevel 2> /dev/null", "r")
+  local git_root = handle:read()
+
+  local source, prompt, root
+  if git_root then
+    source = 'git ls-files --cached --modified --others --exclude-standard'
+    prompt = 'git> '
+    root = git_root
+  else
+    source = 'ag -g "" --hidden | sort'
+    prompt = 'pwd> '
+    root = vim.fn.getcwd()
+  end
+
+  local cmd = string.format(
+    ":call fzf#run(fzf#wrap({'source': '%s', 'options': ['--prompt', '%s'], 'dir': '%s'}))",
+    source,
+    prompt,
+    root
+  )
+  vim.cmd(cmd)
+end
+
+-- Ctrl-P / Leader-P for file navigation
+--
+-- For legacy reasons, both stay around, though the now do the same thing (and
+-- are very smart about it).
+--
+-- If in a git repo, shows a file-picker for all non-ignored files. Untracked
+-- failes are included too. If outside a git repo, just all files. Dotfiles are
+-- always included.
+--
 -- TODO: It'd be great if matches in pwd are weighted higher.
 vim.api.nvim_set_keymap(
   'n',
   '<c-p>',
-  [[(len(system('git rev-parse')) ? ':Files' : ':GFiles')."\<cr>"]],
-  { noremap = true, silent = true, expr = true}
+  [[:lua require('_fzf').show_fuzzy_finder()<cr>]],
+  { noremap = true, silent = true, expr = false}
 )
-
--- This only works on the current directory, and not the entire project.
--- Leader-P for file navigation
--- TODO: Ideally, I'd want this to be the git-directory to the current file.
--- TODO: Should also show git-untracked files.
 vim.api.nvim_set_keymap(
   'n',
   '<Leader>p',
-  [[:Files<cr>]],
+  [[:lua require('_fzf').show_fuzzy_finder()<cr>]],
   { noremap = true, silent = true, expr = false}
 )
 
 -- Hide ruler and line numbers and stuff from fzf window.
 vim.cmd('autocmd FileType fzf set noshowmode noruler nospell nonu nornu')
+
+return M
