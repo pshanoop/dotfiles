c4c82c91ab4309dcbd5d54962423e216b25f0974
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-05-10 08:27:51 +0200

    nvim: Fix configuration for indentline
    Still not 100% right though, due to upstream bugs.

--- home/.config/nvim/lua/_indentlines.lua
+++ home/.config/nvim/lua/_indentlines.lua
@@ -1,10 +1,4 @@
-vim.g.indent_blankline_space_char = '·'
 vim.g.indent_blankline_show_first_indent_level = false
-
-vim.g.indent_blankline_char_highlight = 'IndentLine' -- 606060
-vim.g.indent_blankline_space_char_highlight = 'IndentSpace'
-
--- this is nice, but only if we can alos exclude th spaces
--- let g:indent_blankline_show_trailing_blankline_indent = v:false
-
--- vim.g.indent_blankline_space_char_blankline = ' '
+vim.g.indent_blankline_space_char = '·'
+vim.g.indent_blankline_space_char_blankline = ' '
+-- FIXME: Tabs are rendered as eight spaces (e.g.: for Makefiles)
