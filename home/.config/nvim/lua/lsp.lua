b0b0aafbac56b86c22397f1fc36e275335459a73
Hugo Osvaldo Barrera <hugo@barrera.io>
2021-05-24 22:11:55 +0200

    nvim: Add hotkey for previous diagnostic

--- home/.config/nvim/lua/lsp.lua
+++ home/.config/nvim/lua/lsp.lua
@@ -16,6 +16,7 @@ local on_attach = function(client, bufnr)
 
   -- buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   buf_set_keymap('n', '<Leader>n', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', '<Leader>N', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
 
   buf_set_keymap("n", "<space>a", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
   buf_set_keymap("v", "<space>a", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
